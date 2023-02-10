7842cbd946e26ce27e383269bbad7a1e1280c3e4
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-10 14:40:10 +0100

    Create trains instance DB

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// Import DB
+use App\Models\Train;
+
+
 class PageController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $trainList = Train::all();
+        return view('home', compact($trainList));
     }
 }
